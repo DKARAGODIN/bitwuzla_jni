64f984f8b4d314c7b7dfb13a3993be79ed3e391e
Dmitrii.Karagodin <4319788@gmail.com>
2022-11-16 21:26:50 +0300

    New instance of bitwuzla, True term

---
+++ bitwuzla_jni.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+#include <bitwuzla.h>
+#include "bitwuzla_jni.hpp"
+
+JNIEXPORT jobject JNICALL Java_org_ksmt_solver_bitwuzla_bindings_NativeJNI_bitwuzla_1new
+(JNIEnv* env, jobject zhis) {
+    Bitwuzla* bw = bitwuzla_new();
+    jobject o = env->NewDirectByteBuffer((void*)bw, sizeof(Bitwuzla*));
+    return o;
+}
+
+JNIEXPORT jobject JNICALL Java_org_ksmt_solver_bitwuzla_bindings_NativeJNI_bitwuzla_1mk_1true
+(JNIEnv* env, jobject zhis, jobject bitwuzla) {
+    Bitwuzla* bw = (Bitwuzla*) env->GetDirectBufferAddress(bitwuzla);
+    const BitwuzlaTerm* term = bitwuzla_mk_true(bw);
+    jobject o = env->NewDirectByteBuffer((void*)bw, sizeof(BitwuzlaTerm*));
+    return o;
+}
---
+++ bitwuzla_jni.hpp
@@ -0,0 +1,18 @@
+﻿#include <jni.h>
+
+#ifndef _Included_bitwuzla_jni
+#define _Included_bitwuzla_jni
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	JNIEXPORT jobject JNICALL Java_org_ksmt_solver_bitwuzla_bindings_NativeJNI_bitwuzla_1new
+	(JNIEnv* env, jobject zhis);
+
+	JNIEXPORT jobject JNICALL Java_org_ksmt_solver_bitwuzla_bindings_NativeJNI_bitwuzla_1mk_1true
+	(JNIEnv* env, jobject zhis, jobject bitwuzla);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
